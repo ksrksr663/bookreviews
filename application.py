76ba55c0e52ab67293f29675a44da341c0b01ce6
Robby Renz <nxgrenz@gmail.com>
2019-10-23 23:12:07 -0500

    add code that imports user to database and hashes p/w

--- application.py
+++ application.py
@@ -5,11 +5,13 @@ from flask_session import Session
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 
+from werkzeug.security import generate_password_hash, check_password_hash
+
 app = Flask(__name__)
 
 # Check for environment variable
-# if not os.getenv("DATABASE_URL"):
-    # raise RuntimeError("DATABASE_URL is not set")
+if not os.getenv("DATABASE_URL"):
+    raise RuntimeError("DATABASE_URL is not set")
 
 # Configure session to use filesystem
 app.config["SESSION_PERMANENT"] = False
@@ -17,8 +19,8 @@ app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
 # Set up database
-# engine = create_engine(os.getenv("DATABASE_URL"))
-# db = scoped_session(sessionmaker(bind=engine))
+engine = create_engine(os.getenv("DATABASE_URL"))
+db = scoped_session(sessionmaker(bind=engine))
 
 
 @app.route("/")
@@ -33,11 +35,18 @@ def register_page():
 
 @app.route("/registration", methods=["POST"])
 def registration():
+    # grab user's info from the form
     first_name = request.form.get("first_name")
     last_name = request.form.get("last_name")
     username = request.form.get("username")
     password = request.form.get("password")
-    return "<h1>Success!</h1>"
+    hashed_password = generate_password_hash(password)  # hash the password
+
+    # insert user's info into the database
+    db.execute("INSERT INTO users (first_name, last_name, username, password) VALUES (:first_name, :last_name, :username, :password)", {"first_name": first_name, "last_name": last_name, "username": username, "password": hashed_password})
+    db.commit()  # finalize the transaction
+
+    return render_template("success.html", foo="registered", username=username)
 
 
 if __name__ == "__main__":
