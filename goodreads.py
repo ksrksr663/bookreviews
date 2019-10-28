45d7993461e712d59a69c9d604c69e8ddb4d21a0
Robby Renz <nxgrenz@gmail.com>
2019-10-27 20:38:51 -0500

    goodreads.py now returns a dict of book's info from Goodreads

--- goodreads.py
+++ goodreads.py
@@ -14,7 +14,7 @@ set_environ_var.api_key_goodreads()
 # KEY = os.getenv("API_KEY_GOODREADS")  # read its value
 KEY = os.environ["API_KEY_GOODREADS"]  # -> you can also code it like this
 REQ_URL = "https://www.goodreads.com/book/review_counts.json"
-ISBN = "0553588486"  # the ISBN number for 'A Game of Thrones"
+# ISBN = "0553588486"  # the ISBN number for 'A Game of Thrones"
 
 
 def main(ISBN):
@@ -28,7 +28,14 @@ def main(ISBN):
 
     number_of_rating = res['books'][0]['work_ratings_count']
     average_rating = res['books'][0]['average_rating']
-    print("number of ratings:", number_of_rating, "\naverage rating:", average_rating)
+    # print("number of ratings:", number_of_rating, "\naverage rating:", average_rating)
+
+    # TODO: maybe use a named tuple from the collections module instead of a dictionary
+    book_info = {
+        "number of ratings": number_of_rating,
+        "average rating": average_rating
+    }
+    return book_info
 
 
 if __name__ == "__main__":
